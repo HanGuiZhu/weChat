8dfde9a1d9aa02380dcd9f17149d6ca5867e743f
hgz <18051739494@163.com>
2016-09-26 10:08:32 +0800

    创建了Dog类

---
+++ weChat/weChat/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weChat
+//
+//  Created by 韩桂铸 on 16/9/26.
+//  Copyright © 2016年 韩桂铸. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weChat/weChat/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weChat
+//
+//  Created by 韩桂铸 on 16/9/26.
+//  Copyright © 2016年 韩桂铸. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
